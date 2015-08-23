ba9bdc959e6941ba907bf3082d86016ae14b56b9
Dail Magee Jr <dailma@msn.com>
2015-08-23 13:10:59 -0700

    Added code to both functions; added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## By using the makeCacheMatrix and cacheSolve functions, you can
+## not only invert a matrix but also avoid recalculation of a
+## previously calculated inverse. The larger your matrix, the more
+## processing time you save by inverting once and retrieving the
+## cached matrix subsequently.
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix function accepts an invertible matrix and
+## returns a list that can be used by the cacheSolve function
+## (which see) to invert the matrix and cache the result--or, if
+## the inverse was previously calculated, to retrieve the inverse
+## from the cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        mi <- NULL
+        set <- function(y) {
+                x <<- y
+                mi <<- NULL
+        }
+        get <- function() x
+        setMI <- function(solve) mi <<- solve
+        getMI <- function() mi
+        list(set = set, get = get,
+             setMI = setMI,
+             getMI = getMI)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function accepts a list from the makeCacheMatrix
+## function (which see) and determines whether the inverse has
+## already been calculated. If so, cacheSolve retrieves the
+## inverse from the cache; if not, cacheSolve uses solve() to
+## invert the matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        mi <- x$getMI()
+        if(!is.null(mi)) {
+                message("retrieving cached data")
+                return(mi)
+        }
+        data <- x$get()
+        mi <- solve(data, ...)
+        x$setMI(mi)
+        mi
 }
